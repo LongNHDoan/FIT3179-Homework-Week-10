a1f1bf3e8934d6b622373da3d80be0bbc599789e
LongNHDoan <91375990+LongNHDoan@users.noreply.github.com>
2021-10-10 01:11:59 +0700

    Add files via upload

--- myJavaScript.js
+++ myJavaScript.js
@@ -1,2 +1,4 @@
 var vg_1 = "IndustriesC02Graph.vl.json";
+var vg_2 = "CountryCO2Map.vl.json";
 vegaEmbed("#area_chart", vg_1).then(function (result) { }).catch(console.error);
+vegaEmbed("#map", vg_2).then(function (result) { }).catch(console.error);
